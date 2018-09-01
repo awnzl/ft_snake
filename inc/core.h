93a5ac1034bd3ad6476fc666db37a27fac1a6a4a
Alexander Wenzel <al.w.postbox@gmail.com>
2018-09-01 18:15:05 +0300

    add image loader

--- inc/core.h
+++ inc/core.h
@@ -82,7 +82,7 @@ class GameCore
     void    updateSnake(int nx, int ny);
     void    updateTarget(Block *target);
     void    increaseSnake(int nx, int ny);
-    
+
     std::uint8_t	*getImage(std::uint8_t *pixels);
 
     bool    checkTarget(int x, int y, Block* target);
