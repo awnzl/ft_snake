dea5766eaff13ff73e09b6a0df34bef171886ccf
Alexander Wenzel <al.w.postbox@gmail.com>
2018-09-13 20:19:07 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,10 @@ void printUsage()
               << "         food, it becomes longer.\n"
               << "\n"
               << "         To start the game, you must specify the width and the height of the playing field.\n"
-              << "         For example: ./nibbler 40 40\n"
+              << "         For example: ./nibbler 20 10\n"
               << "\n"
-              << "         Minimum field sizes: 30x30 \n"
-              << "         Maximum field sizes: 99х99\n"
+              << "         Minimum field sizes: 10x10 \n"
+              << "         Maximum field sizes: 50х50\n"
               << "         Snake control, keys: UP, DOWN, RIGHT, LEFT\n"
               << "         To exit the game:    ESC\n"
               << "\n";
@@ -39,8 +39,8 @@ int main(int ac, char *av[])
     try
     {
         std::string exceptionMessage("\nEXEPTION: Incorrect value of field sizes.\n"
-                                      "Minimum field sizes: 30x30 \n"
-                                      "Maximum field sizes: 99х99\n");
+                                      "Minimum field sizes: 10x10\n"
+                                      "Maximum field sizes: 50х50\n");
         if (std::regex_match(s1.c_str(), result, regular) &&
             std::regex_match(s2.c_str(), result, regular) &&
             std::regex_match(s3.c_str(), result, regular))
@@ -48,7 +48,7 @@ int main(int ac, char *av[])
             width = std::stoi(av[1]);
             height = std::stoi(av[2]);
             gameMode = std::stoi(s3);
-            if(width < 10 || height < 10 || width > 61 || height > 61 || gameMode > 2)
+            if(width < 10 || height < 10 || width > 50 || height > 50 || gameMode > 2)
                 throw std::invalid_argument(exceptionMessage);
             GameCore *gc = new GameCore(width, height, gameMode);
             // timer.setTimeScale(0.2f);//TODO: replace by value of mandatory's requiroment
